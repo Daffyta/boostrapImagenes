d4d0b6ff271134aa093e90c9ee7f6af9331cfee2
Daffyta <daffy.dine9@gmail.com>
2017-04-25 17:10:14 -0500

    Agregando estilo h2

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ var div = document.createElement('div');
 var h2 = document.createElement('h2');
 var h3 = document.createElement('h3');
 //asignar estilos a div
-div.style.border = "1px solid red";
-div.style.height = "500px";
-div.style.width = "500px";
+// div.style.border = "1px solid red";
+// div.style.height = "500px";
+// div.style.width = "500px";
 // asignar estilos a h2
 h2.className = "h2Dz";
 h2.innerText = "Designed for everyone,everywhere.";
