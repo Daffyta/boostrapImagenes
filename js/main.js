df670e0c4873bccb3e50575bbcf1aa83bc2b1877
nancyzj <ancizj@hotmail.com>
2017-04-25 14:13:43 -0500

    agregue h2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-alert("hol");
+///crear variables
+var contenedorDz = document.getElementById('contenedorDz');
+
+//crear elementos
+var div = document.createElement('div');
+var h2 = document.createElement('h2');
+var h3 = document.createElement('h3');
+//asignar estilos a div
+div.style.border = "1px solid red";
+div.style.height = "500px";
+div.style.width = "500px";
+// asignar estilos a h2
+h2.className = "h2Dz";
+h2.innerText = "Designed for everyone,everywhere.";
+
+
+//agregar al html
+contenedorDz.appendChild(div);
+div.appendChild(h2);
